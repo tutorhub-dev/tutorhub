32f3b029201a4d815526711975ba7ebc94954def
Mohamed-Darbi <darbimo@hotmail.com>
2024-03-05 21:36:53 -0600

    test

--- src/main.js
+++ src/main.js
@@ -1,6 +1,23 @@
 const express = require('express')
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+dotenv.config();
+
+
 const app = express()
 const port = 3000
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MDB_URL);
+        console.log("Database Connected");
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        Process.exit(1);
+    }
+}
+
+connectDB();
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -9,3 +26,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
