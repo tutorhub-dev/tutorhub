e720079c55252ad56c7e8c55fea2b73f7fce216b
mitsukaki <lite260@gmail.com>
2024-03-06 21:12:53 -0600

    tutorhub backend API

---
+++ src/api/api.js
@@ -0,0 +1,31 @@
+class TutorAPI
+{
+    constructor(apiConfig)
+    {
+        this.apiConfig = apiConfig;
+    }
+
+    login(req, res) {
+        res.status(501).send('Not Implemented')
+    }
+
+    logout(req, res) {
+        res.status(501).send('Not Implemented')
+    }
+
+    getUser(req, res) {
+        res.status(501).send('Not Implemented')
+    }
+
+    createUser(req, res) {
+        res.status(501).send('Not Implemented')
+    }
+
+    updateUser(req, res) {
+        res.status(501).send('Not Implemented')
+    }
+
+    deleteUser(req, res) {
+        res.status(501).send('Not Implemented')
+    }
+}
