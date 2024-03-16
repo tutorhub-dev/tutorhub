63fd0fc9ad0fbec35bf3485f21b339a6abc6aaa9
egalized00 <ibrahimegal6@gmail.com>
2024-03-16 00:14:58 -0600

    Tailwind css
    Added tailwind config and some css files

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './public/**/*.{html,js}',
+    './src/**/*.{html,js}',
+    './public/index.html',
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
