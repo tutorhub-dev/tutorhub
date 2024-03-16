f80fd3a86ac659ded8a4b31485fae7ed1e91b1b5
mitsukaki <lite260@gmail.com>
2024-03-16 14:10:18 -0600

    fix: resolve merge conflict

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-<<<<<<< HEAD
   content: ["./public/*.html"],
-=======
-  content: [
-    './public/**/*.{html,js}',
-    './src/**/*.{html,js}',
-    './public/index.html',
-],
->>>>>>> 5a2c4cf76ee11b2a86744fc3eb2930e2112ff9e1
   theme: {
     extend: {},
   },
   plugins: [],
-<<<<<<< HEAD
 }
 
 
 
-=======
-}
->>>>>>> 5a2c4cf76ee11b2a86744fc3eb2930e2112ff9e1
